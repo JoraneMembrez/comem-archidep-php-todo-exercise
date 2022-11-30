6e20a5f0f12828b8cb09a8d792739887a3d8e4be
Jorane Membrez <jorane.membrez@outlook.com>
2022-11-30 12:51:51 +0100

    add env variables

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
 
 // Database connection parameters.
-define('DB_USER', getenv('TODOLIST_BD_USER'));
-define('DB_PASS', getenv('TODOLIST_DB_PASS'));
-define('DB_NAME', getenv('TODOLIST_BD_NAME'));
-define('DB_HOST', getenv('TODLIST_BD_HOST'));
-define('DB_PORT', getenv('TODLIST_BD_PORT'));
+define('DB_USER', getenv('TODOLIST_BD_USER') ?: 'todolist');
+define('DB_PASS', getenv('TODOLIST_DB_PASS')); // pour pas que les gens connaissent notre mot de passe
+define('DB_NAME', getenv('TODOLIST_BD_NAME')?: 'todolist');
+define('DB_HOST', getenv('TODLIST_BD_HOST') ?: '127.0.0.1');
+define('DB_PORT', getenv('TODLIST_BD_PORT') ?: '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
